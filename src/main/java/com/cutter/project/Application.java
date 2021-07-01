1d204ae797cda727e4aec027998b96c2dc2ff9d7
sorokaa <sasha20999@outlook.com>
2021-07-02 00:01:11 +0300

    Refactor main class

--- src/main/java/com/cutter/project/Application.java
+++ src/main/java/com/cutter/project/Application.java
@@ -4,9 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ApplicationCutter {
+public class Application {
     public static void main(String[] args) {
-        SpringApplication.run(ApplicationCutter.class, args);
+        SpringApplication.run(Application.class, args);
     }
-
 }
